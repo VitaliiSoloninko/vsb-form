5c128521a933bb9c6d813892ea29f17ab53bd1c1
Vitalii Soloninko <130390124+VitaliiSoloninko@users.noreply.github.com>
2026-02-04 18:53:59 +0100

    header, content, footer

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import {
+  IonButton,
+  IonContent,
+  IonFooter,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonFooter, IonButton],
 })
 export class HomePage {
   constructor() {}
