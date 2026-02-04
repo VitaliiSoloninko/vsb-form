1e38d07b87622c8bb87afb1fb14aabb19ef9694d
Vitalii Soloninko <130390124+VitaliiSoloninko@users.noreply.github.com>
2026-02-04 19:02:58 +0100

    language switch (DE, EN, RU, UA, AR)

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -15,5 +15,21 @@ import {
   imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonFooter, IonButton],
 })
 export class HomePage {
+  currentLanguage = 'de';
+
+  languages = [
+    { code: 'de', name: 'Deutsch' },
+    { code: 'en', name: 'English' },
+    { code: 'ru', name: 'Русский' },
+    { code: 'ar', name: 'العربية' },
+    { code: 'ua', name: 'Українська' },
+  ];
+
   constructor() {}
+
+  changeLanguage(langCode: string) {
+    this.currentLanguage = langCode;
+    console.log('Language changed to:', langCode);
+    // i18n will be implemented in Step 16
+  }
 }
