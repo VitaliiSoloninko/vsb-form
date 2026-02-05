1d2c5b6eca43b51d02bec28a621da65179d6f247
Vitalii Soloninko <130390124+VitaliiSoloninko@users.noreply.github.com>
2026-02-05 11:17:41 +0100

    Add 6 form fields for Step 1 personal data

--- src/app/components/steps/step1-personal/step1-personal.component.ts
+++ src/app/components/steps/step1-personal/step1-personal.component.ts
@@ -1,14 +1,33 @@
 import { Component, input } from '@angular/core';
 import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import {
+  IonInput,
+  IonItem,
+  IonLabel,
+  IonSelect,
+  IonSelectOption,
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-step1-personal',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [
+    ReactiveFormsModule,
+    IonItem,
+    IonLabel,
+    IonInput,
+    IonSelect,
+    IonSelectOption,
+  ],
   templateUrl: './step1-personal.component.html',
   styleUrls: ['./step1-personal.component.scss'],
 })
 export class Step1PersonalComponent {
   formGroup = input.required<FormGroup>();
-  // Reactive form fields will be added in Step 7
+
+  maritalStatuses = [
+    { value: 'single', label: 'Single' },
+    { value: 'married', label: 'Married' },
+    { value: 'divorced', label: 'Divorced' },
+  ];
 }
