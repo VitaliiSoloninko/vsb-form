33ccd52c5904c1ed1223b8ef03dbd3c39b656e61
Vitalii Soloninko <130390124+VitaliiSoloninko@users.noreply.github.com>
2026-02-06 09:53:04 +0100

    Add 6 form fields for contacts and addresses, divided into two sections

--- src/app/components/steps/step2-contact/step2-contact.component.ts
+++ src/app/components/steps/step2-contact/step2-contact.component.ts
@@ -1,14 +1,14 @@
 import { Component, input } from '@angular/core';
 import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { IonInput, IonItem, IonLabel } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-step2-contact',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, IonItem, IonLabel, IonInput],
   templateUrl: './step2-contact.component.html',
   styleUrls: ['./step2-contact.component.scss'],
 })
 export class Step2ContactComponent {
   formGroup = input.required<FormGroup>();
-  // Reactive form fields will be added in Step 8
 }
